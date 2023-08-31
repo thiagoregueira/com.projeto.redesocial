dbc2620870902396c5e7bdd669ee529b9f03e724
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-08-31 12:49:15 -0300

    Criação da classe Comment, alteração do banner e inclusão do arquivo sql no gitignore.

---
+++ src/main/java/com/projeto/redesocial/entity/Comment.java
@@ -0,0 +1,51 @@
+package com.projeto.redesocial.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments", schema = "redesocial")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comments_sq")
+    @SequenceGenerator(schema = "redesocial", name = "comments_sq", sequenceName = "comments_sq", initialValue = 1, allocationSize = 1)
+    private Long id;
+
+    private String comment;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "posts_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    private User user;
+
+}
